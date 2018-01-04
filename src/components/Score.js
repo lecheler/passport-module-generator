15697a15fd71e29566cd8e6b06095e0c07f510db
Justin Eder <justineder@gmail.com>
2018-01-04 15:09:00 -0600

    category scores updating properly

---
+++ src/components/Score.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { withStyles } from "material-ui/styles";
+import InputFieldTwo from "./InputFieldTwo";
+import IconButton from "material-ui/IconButton";
+import DeleteIcon from "material-ui-icons/Delete";
+
+const styles = theme => ({
+  root: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  button: {
+    margin: theme.spacing.unit
+  }
+});
+
+function Score(props) {
+  const { classes, scoring, catIndex, scoreIndex } = props;
+
+  const handleMaxUpdate = input => {
+    props.scoring.update(catIndex, scoreIndex, { max: input.value });
+  };
+
+  const handleLabelUpdate = input => {
+    props.scoring.update(catIndex, scoreIndex, { label: input.value });
+  };
+
+  const handleDelete = () => {
+    props.scoring.remove(catIndex, scoreIndex);
+  };
+
+  return (
+    <div className={classes.root}>
+      <InputFieldTwo
+        label={`Score ${scoreIndex + 1}`}
+        handleChange={handleLabelUpdate}
+        placeholder="Score Label"
+        value={props.scoreContent.label}
+      />
+      <InputFieldTwo
+        label="Max"
+        handleChange={handleMaxUpdate}
+        placeholder="Score"
+        value={props.scoreContent.max}
+      />
+      <IconButton
+        className={classes.button}
+        aria-label="Delete"
+        onClick={handleDelete}
+      >
+        <DeleteIcon />
+      </IconButton>
+    </div>
+  );
+}
+
+export default withStyles(styles)(Score);
