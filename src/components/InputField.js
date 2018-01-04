b132a9f8246208c2bf65a3964b3e2b459e23a0a9
Jake Sullivan <jakefsullivan@gmail.com>
2018-01-04 15:09:29 -0600

    modified inputfield input default value

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -41,7 +41,7 @@ class InputField extends Component {
       <div className={this.props.classes.root}>
         <Input
           onChange={this.props.handleChange}
-          defaultValue={this.props.title}
+          defaultValue={this.props.inputValue}
           placeholder={this.props.placeholder}
           className={this.props.classes.input}
           inputProps={{
